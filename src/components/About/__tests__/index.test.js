97c3f18945d0e76cd961bbe2dfe7047d7c7b0a74
khyan22 <khyanalvarez@gmail.com>
2022-11-20 21:53:03 -0500

    added About component test js file, switched testing dependencies to devDependencies

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+//* the '..' indicates the directory above
+import About from '..';
+
+afterEach(cleanup);
+
+describe('About component', () => {
+  
+})
