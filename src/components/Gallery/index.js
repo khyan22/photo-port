e1e4a893acf3381dc84160177993218533ffe0fb
khyan22 <khyanalvarez@gmail.com>
2023-01-11 01:04:41 -0500

    added gallery component

---
+++ src/components/Gallery/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+import photo from '../../assets/small/commercial/0.jpg'
+
+function Gallery(props) {
+  const currentCategory = {
+    name: 'Commercial',
+    description: 'Photo of grocery stores, food truck, and other commercial projects'
+  };
+
+  return (
+    <section>
+      <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
+      <p>{currentCategory.description}</p>
+      <div className='flex-row'> 
+        <img
+          src={photo}
+          alt='Commercial Example'
+          className='img-thumbnail mx-1'
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Gallery;
