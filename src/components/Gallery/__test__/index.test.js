44a944a4b6b282dcedfba227e80237c86f121e24
khyan22 <khyanalvarez@gmail.com>
2023-01-19 20:01:56 -0500

    added gallery test and passed them

--- src/components/Gallery/__test__/index.test.js
+++ src/components/Gallery/__test__/index.test.js
@@ -11,4 +11,14 @@ describe('gallery is rendering', () => {
   it('renders', () => {
     render(<Gallery currentCategory={portrait} />);
   });
+
+  it('matches snapshot', () => {
+    const { asFragment } = render(<Gallery currentCategory={portrait} />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
+
+it('renders', () => {
+  const { getByTestId } = render(<Gallery currentCategory={portrait} />);
+  expect(getByTestId('h1tag')).toHaveTextContent('Portraits');
 });
