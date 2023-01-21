e91174e701b2af3a508b6d28a5fda885cfea4383
khyan22 <khyanalvarez@gmail.com>
2023-01-20 19:07:23 -0500

    created ContactForm component

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -9,17 +9,14 @@ function ContactForm() {
           <label htmlFor='name'>Name:</label>
           <input type='text' name='name'/>
         </div>
-
         <div>
           <label htmlFor='email'>Email:</label>
           <input type='email' name='email'/>
         </div>
-
         <div>
           <label htmlFor='message'>Message:</label>
           <input type='text' name='message'/>
         </div>
-
         <button type='submit'>Submit</button>
       </form>
     </section>
